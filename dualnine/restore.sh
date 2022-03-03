67c27c49ff96650cbc5fc5b8f93938dcbce2c5f2
Wiley Lau <henloboii@protonmail.ch>
2022-03-03 11:25:09 +0800

    dualnine: cleanup active slot after restore
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- dualnine/restore.sh
+++ dualnine/restore.sh
@@ -4,4 +4,4 @@ pigz -d -c /data/dualnine/active/boot.img.gz > /dev/block/by-name/boot;
 pigz -d -c /data/dualnine/active/dtbo.img.gz > /dev/block/by-name/dtbo;
 pigz -d -c /data/dualnine/active/super.img.gz > /dev/block/by-name/super;
 mv /data/dualnine/active/data/* /data/
-rm -rf /data/dualnine/active/;
+rm -rf /data/dualnine/active/
