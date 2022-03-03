124cd31ed7af970cbe33f6b8b08a52ffe4d9a5d9
Wiley Lau <henloboii@protonmail.ch>
2022-03-03 12:11:00 +0800

    dualnine: add EXPERIMENTAL VirtualAB Support
    * UNTESTED!!!!!
    * credit roxirp on TG for grabbing slot prop for me
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- dualnine/backup.sh
+++ dualnine/backup.sh
@@ -1,10 +1,14 @@
 #!/sbin/sh
 
+SLOT=$(getprop ro.boot.slot_suffix)
 mv /data/dualnine/inactive/ /data/dualnine/active/;
 mkdir -p /data/dualnine/inactive/data/;
-pigz < /dev/block/by-name/boot > /data/dualnine/inactive/boot.img.gz
-pigz < /dev/block/by-name/dtbo > /data/dualnine/inactive/dtbo.img.gz
+pigz < "/dev/block/by-name/boot$SLOT" > /data/dualnine/inactive/boot.img.gz
+pigz < "/dev/block/by-name/dtbo$SLOT" > /data/dualnine/inactive/dtbo.img.gz
 pigz < /dev/block/by-name/super > /data/dualnine/inactive/super.img.gz
+if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
+    pigz < "/dev/block/by-name/vendor_boot$SLOT" > /data/dualnine/inactive/vendor_boot.img.gz
+fi
 ui_print "Saving userdata for current slot"
 mv /data/* /data/dualnine/inactive/data/
 echo "1" > /data/dualnine/params/inactive;
--- dualnine/restore.sh
+++ dualnine/restore.sh
@@ -1,7 +1,11 @@
 #!/sbin/sh
 
-pigz -d -c /data/dualnine/active/boot.img.gz > /dev/block/by-name/boot;
-pigz -d -c /data/dualnine/active/dtbo.img.gz > /dev/block/by-name/dtbo;
+SLOT=$(getprop ro.boot.slot_suffix)
+pigz -d -c /data/dualnine/active/boot.img.gz > "/dev/block/by-name/boot$SLOT";
+pigz -d -c /data/dualnine/active/dtbo.img.gz > "/dev/block/by-name/dtbo$SLOT";
 pigz -d -c /data/dualnine/active/super.img.gz > /dev/block/by-name/super;
+if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
+    pigz -d -c /data/dualnine/active/vendor_boot.img.gz > "/dev/block/by-name/vendor_boot$SLOT";
+fi
 mv /data/dualnine/active/data/* /data/
 rm -rf /data/dualnine/active/
