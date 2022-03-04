12ff5fd5794a1c505b8050d523ce68ba7b45bd07
Wiley Lau <henloboii@protonmail.ch>
2022-03-04 15:44:47 +0800

    dualnine: add EXPERIMENTAL static partitions support
    * really untested
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- dualnine/restore.sh
+++ dualnine/restore.sh
@@ -3,9 +3,17 @@
 SLOT=$(getprop ro.boot.slot_suffix)
 pigz -d -c /data/dualnine/active/boot.img.gz > "/dev/block/by-name/boot$SLOT";
 pigz -d -c /data/dualnine/active/dtbo.img.gz > "/dev/block/by-name/dtbo$SLOT";
-pigz -d -c /data/dualnine/active/super.img.gz > /dev/block/by-name/super;
+
+if [[ -f /dev/block/by-name/super ]]; then
+    pigz -d -c /data/dualnine/active/super.img.gz > /dev/block/by-name/super;
+else
+    pigz -d -c /data/dualnine/active/system.img.gz > "/dev/block/by-name/system$SLOT";
+    pigz -d -c /data/dualnine/active/vendor.img.gz > "/dev/block/by-name/vendor$SLOT";
+fi
+
 if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
     pigz -d -c /data/dualnine/active/vendor_boot.img.gz > "/dev/block/by-name/vendor_boot$SLOT";
 fi
+
 mv /data/dualnine/active/data/* /data/
 rm -rf /data/dualnine/active/
