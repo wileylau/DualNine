24745d50cb865481a5053ca8c5053e6a5df8e7ed
Wiley Lau <henloboii@protonmail.ch>
2022-03-05 10:35:36 +0800

    dualnine: Script cleanups
    * fix conditions for checking super / vendor_boot existence
    * drop all ui_print to prevent logspam
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- dualnine/backup.sh
+++ dualnine/backup.sh
@@ -6,18 +6,17 @@ mkdir -p /data/dualnine/inactive/data/;
 pigz < "/dev/block/by-name/boot$SLOT" > /data/dualnine/inactive/boot.img.gz
 pigz < "/dev/block/by-name/dtbo$SLOT" > /data/dualnine/inactive/dtbo.img.gz
 
-if [[ -f "/dev/block/by-name/super" ]]; then
+if [[ -e "/dev/block/by-name/super" ]]; then
     pigz < /dev/block/by-name/super > /data/dualnine/inactive/super.img.gz
 else
     pigz < "/dev/block/by-name/system$SLOT" > /data/dualnine/inactive/system.img.gz
     pigz < "/dev/block/by-name/vendor$SLOT" > /data/dualnine/inactive/vendor.img.gz
 fi
 
-if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
+if [[ -e "/dev/block/by-name/vendor_boot$SLOT" ]]; then
     pigz < "/dev/block/by-name/vendor_boot$SLOT" > /data/dualnine/inactive/vendor_boot.img.gz
 fi
 
-ui_print "Saving userdata for current slot"
 mv /data/* /data/dualnine/inactive/data/
 echo "1" > /data/dualnine/params/inactive;
 
--- dualnine/init.sh
+++ dualnine/init.sh
@@ -4,10 +4,9 @@ if [[ ! -f /data/dualnine/params/slot ]]; then
     mkdir -p /data/dualnine/params/;
     touch /data/dualnine/params/slot;
     echo "a" > /data/slot/params/slot;
-    ui_print " ";
     setprop ro.dualnine.slot a;
 fi
-if [[ -f /dev/block/by-name/super ]]; then
+if [[ -e /dev/block/by-name/super ]]; then
     echo "dynamic" > /data/dualnine/params/device_type;
 else
     echo "static" > /data/dualnine/params/device_type;
