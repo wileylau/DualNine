f47d38cf3dd7285e79b59d622459e0e51975a621
Wiley Lau <henloboii@protonmail.ch>
2022-09-20 08:56:12 +0800

    Use D9L variable to determine DualNine work directory.

--- dualnine/backup.sh
+++ dualnine/backup.sh
@@ -1,18 +1,19 @@
 #!/sbin/sh
 
 SLOT=$(getprop ro.boot.slot_suffix)
-mv /data/dualnine/inactive/ /data/dualnine/active/;
-mkdir -p /data/dualnine/inactive/data/;
-pigz < "/dev/block/by-name/boot$SLOT" > /data/dualnine/inactive/boot.img.gz
-pigz < "/dev/block/by-name/dtbo$SLOT" > /data/dualnine/inactive/dtbo.img.gz
-pigz < /dev/block/by-name/super > /data/dualnine/inactive/super.img.gz
+D9L="$D9L"
+mv $D9L/inactive/ $D9L/active/;
+mkdir -p $D9L/inactive/data/;
+pigz < "/dev/block/by-name/boot$SLOT" > $D9L/inactive/boot.img.gz
+pigz < "/dev/block/by-name/dtbo$SLOT" > $D9L/inactive/dtbo.img.gz
+pigz < /dev/block/by-name/super > $D9L/inactive/super.img.gz
 if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
-    pigz < "/dev/block/by-name/vendor_boot$SLOT" > /data/dualnine/inactive/vendor_boot.img.gz
+    pigz < "/dev/block/by-name/vendor_boot$SLOT" > $D9L/inactive/vendor_boot.img.gz
 fi
 ui_print "Saving userdata for current slot"
-mv /data/* /data/dualnine/inactive/data/
-echo "1" > /data/dualnine/params/inactive;
+mv /data/* $D9L/inactive/data/
+echo "1" > $D9L/params/inactive;
 
-if [[ -f /data/dualnine/active/boot.img.gz ]]; then
+if [[ -f $D9L/active/boot.img.gz ]]; then
     setprop ro.dualnine.restore 1
 fi
