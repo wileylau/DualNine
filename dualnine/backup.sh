bddaae1e44ed5c33f63f4c45a8e977ff087ad086
Wiley Lau <henloboii@protonmail.ch>
2022-09-20 09:02:25 +0800

    Convert to backup-restore script.

--- dualnine/backup.sh
+++ dualnine/backup.sh
@@ -1,19 +1,23 @@
 #!/sbin/sh
 
 SLOT=$(getprop ro.boot.slot_suffix)
-D9L="/data/dualnine"
-mv $D9L/inactive/ $D9L/active/;
-mkdir -p $D9L/inactive/data/;
-pigz < "/dev/block/by-name/boot$SLOT" > $D9L/inactive/boot.img.gz
-pigz < "/dev/block/by-name/dtbo$SLOT" > $D9L/inactive/dtbo.img.gz
-pigz < /dev/block/by-name/super > $D9L/inactive/super.img.gz
-if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
-    pigz < "/dev/block/by-name/vendor_boot$SLOT" > $D9L/inactive/vendor_boot.img.gz
-fi
-ui_print "Saving userdata for current slot"
-mv /data/* $D9L/inactive/data/
-echo "1" > $D9L/params/inactive;
-
-if [[ -f $D9L/active/boot.img.gz ]]; then
-    setprop ro.dualnine.restore 1
+D9L="/sdcard/DualNine"
+if [[ -f $D9L/boot.img.gz ]]; then
+    pigz -d -c $D9L/boot.img.gz > "/dev/block/by-name/boot$SLOT";
+    pigz -d -c $D9L/dtbo.img.gz > "/dev/block/by-name/dtbo$SLOT";
+    pigz -d -c $D9L/super.img.gz > /dev/block/by-name/super;
+    if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
+        pigz -d -c $D9Lvendor_boot.img.gz > "/dev/block/by-name/vendor_boot$SLOT";
+    fi
+    mv $D9Ldata/* /data/
+else
+    mkdir -p $D9L/data/;
+    pigz < "/dev/block/by-name/boot$SLOT" > $D9L/boot.img.gz
+    pigz < "/dev/block/by-name/dtbo$SLOT" > $D9L/dtbo.img.gz
+    pigz < /dev/block/by-name/super > $D9L/super.img.gz
+    if [[ -f "/dev/block/by-name/vendor_boot$SLOT" ]]; then
+        pigz < "/dev/block/by-name/vendor_boot$SLOT" > $D9L/vendor_boot.img.gz
+    fi
+    ui_print "Saving userdata for current slot"
+    mv /data/* $D9L/data/
 fi
